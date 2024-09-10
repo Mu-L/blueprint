b7dfbd4c2a616de3fc505a0a1b9b7b4beceff377
Gregory Douglas <2968519+ggdouglas@users.noreply.github.com>
2024-09-09 22:37:34 -0400

    Refactor ButtonsIconsExample

--- packages/docs-app/src/examples/core-examples/buttonsIconsExample.tsx
+++ packages/docs-app/src/examples/core-examples/buttonsIconsExample.tsx
@@ -19,19 +19,17 @@ import * as React from "react";
 import { Button, Icon } from "@blueprintjs/core";
 import { Example, type ExampleProps } from "@blueprintjs/docs-theme";
 
-export class ButtonsIconsExample extends React.PureComponent<ExampleProps> {
-    public render() {
-        return (
-            <Example options={false} {...this.props}>
-                {/* icon and rightIcon props */}
-                <Button icon="refresh" intent="danger" text="Reset" />
-                <Button icon="user" rightIcon="caret-down" text="Profile settings" />
-                <Button rightIcon="arrow-right" intent="success" text="Next step" />
-                {/* <Icon> children as inline text elements */}
-                <Button>
-                    <Icon icon="document" /> Upload... <Icon icon="small-cross" />
-                </Button>
-            </Example>
-        );
-    }
-}
+export const ButtonsIconsExample: React.FC<ExampleProps> = props => {
+    return (
+        <Example options={false} {...props}>
+            {/* icon and rightIcon props */}
+            <Button icon="refresh" intent="danger" text="Reset" />
+            <Button icon="user" rightIcon="caret-down" text="Profile settings" />
+            <Button rightIcon="arrow-right" intent="success" text="Next step" />
+            {/* <Icon> children as inline text elements */}
+            <Button>
+                <Icon icon="document" /> Upload... <Icon icon="small-cross" />
+            </Button>
+        </Example>
+    );
+};
