4caa136f8c059ae96f66cbd62859f6ceed5f9a25
Gregory Douglas <gdouglas@palantir.com>
2024-12-03 17:00:04 -0500

    Fix Enzyme adapter import (again)

--- packages/test-commons/bootstrap.mjs
+++ packages/test-commons/bootstrap.mjs
@@ -2,11 +2,11 @@
  * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
  */
 
-import Adapter from "@cfaester/enzyme-adapter-react-18";
+import ReactEighteenAdapter from "@cfaester/enzyme-adapter-react-18";
 import Enzyme from "enzyme";
 
-const adapter = new Adapter.default();
+const Adapter = ReactEighteenAdapter.default;
 
-Enzyme.configure({ adapter });
+Enzyme.configure({ adapter: new Adapter() });
 
 console.info(`Enzyme configured with *${Adapter.name}*`);
